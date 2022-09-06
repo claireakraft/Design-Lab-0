3271c5efbc96808b0d75883842cebae14096ebef
claireakraft <kraftclaire125@gmail.com>
2022-09-06 15:05:43 -0500

    LED on all the time

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include "mbed.h"
+#include "CKraft_binaryutils.h"
+
+#define set (uint32_t *)0x50000508
+#define clear (uint32_t *)0x5000050C
+#define dir (uint32_t *)0x50000514
+#define pin (uint8_t)13
+
 
 // main() runs in its own thread in the OS
 int main()
 {
-    while (true) {
 
+    setbit(dir, pin);
+    
+
+    while (true) {
+        setbit(set, pin);
     }
 }
 
